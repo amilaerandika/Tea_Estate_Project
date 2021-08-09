6fb96734ba9a53537587017544db3a238b4e2a97
amilaerandika <amilaerandikasampth@gmail.com>
2021-08-09 14:56:19 +0530

    Added Gov Model

--- WindowsFormsApp1/Models/AgreementGov.cs
+++ WindowsFormsApp1/Models/AgreementGov.cs
@@ -8,26 +8,14 @@ namespace WindowsFormsApp1.Models
 {
     public class AgreementGov
     {
-        /*
-         * 
-		private string _Agreement_No;
-		
-		private string _Name;
-		
-		private System.Nullable<System.DateTime> _Gazzate_Date;
-		
-		private string _Type_Of_Agreement;
-		
-		private string _Remarks;
-		
-		private System.Nullable<System.Guid> _Document_ID;
-		
-		private System.Guid _System_User_ID;
-		
-		private System.Nullable<System.DateTime> _Entry_Date;*/
         public string AgreementNo { get; set; }
         public string AgreementName { get; set; }
         public DateTime GazzateDate { get; set; }
+        public string TypeOfAgreement { get; set; }
+        public string Remarks { get; set; }
+        public Guid DocumentId { get; set; }
+        public Guid SystemUserId { get; set; }
+        public DateTime EntryDate { get; set; }
 
     }
 }
