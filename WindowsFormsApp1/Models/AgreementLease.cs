986d1705571a74e830b060bc916c6ab28b622278
amilaerandika <amilaerandikasampth@gmail.com>
2021-08-09 15:47:00 +0530

    Commit

---
+++ WindowsFormsApp1/Models/AgreementLease.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Models
+{
+    public class AgreementLease
+    {
+        public string AgreementId { get; set; }
+        public Guid LeaserId { get; set; }
+        public string PropertyId { get; set; }
+        public string EstateId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Duration { get; set; }
+        public decimal AgreementValue { get; set; }
+        public Guid SystemUserId { get; set; }
+        public DateTime EntryDate { get; set; }
+    }
+}
