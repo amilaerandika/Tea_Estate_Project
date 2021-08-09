c462d1e94d9159f5eca8496035081dd3bb0a428c
amilaerandika <amilaerandikasampth@gmail.com>
2021-08-09 14:26:44 +0530

    all changes

---
+++ WindowsFormsApp1/Models/Address.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Models
+{
+    public class Address
+    {
+        public Guid AddressID { get; set; }
+        public string FromTable { get; set; }
+        public string OriginDescription { get; set; }
+        public string LineOne { get; set; }
+        public string LineTwo { get; set; }
+        public string LineThree { get; set; }
+        public string LineFour { get; set; }
+        public string LineFive { get; set; }
+        public string LineSix { get; set; }
+        public string RestOfTheAddress { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public Guid SystemUserID { get; set; }
+    }
+}
---
+++ WindowsFormsApp1/Models/AgreementGov.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Models
+{
+    public class AgreementGov
+    {
+        /*
+         * 
+		private string _Agreement_No;
+		
+		private string _Name;
+		
+		private System.Nullable<System.DateTime> _Gazzate_Date;
+		
+		private string _Type_Of_Agreement;
+		
+		private string _Remarks;
+		
+		private System.Nullable<System.Guid> _Document_ID;
+		
+		private System.Guid _System_User_ID;
+		
+		private System.Nullable<System.DateTime> _Entry_Date;*/
+        public string AgreementNo { get; set; }
+        public string AgreementName { get; set; }
+        public DateTime GazzateDate { get; set; }
+
+    }
+}
